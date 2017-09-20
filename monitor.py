a0409d8a81dee5fe54e5ce1d7f6e4941e440cad8
Zhaomeng Zhu <zhaomeng.zhu@gmail.com>
2017-09-20 16:54:04 +0800

    start_worker replaces restart and update

--- monitor.py
+++ monitor.py
@@ -12,20 +12,14 @@ class Monitor(RPC):
     def __init__(self, path):
         self.path = os.path.abspath(path)
 
-    def update_worker(self):
-        subprocess.run(["git", "-C", self.path, "pull"])
+    def start_worker(self, update):
+        if update:
+            subprocess.run(["git", "-C", self.path, "pull"])
         self.stop_worker()
-        self.start_worker()
-
-    def start_worker(self):
         subprocess.Popen([os.path.join(self.path, "worker.py")])
 
     def stop_worker(self):
         subprocess.run(["pkill", "-9", "-f", "worker.py"])
-
-    def restart_worker(self):
-        self.stop_worker()
-        self.start_worker()
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -7,12 +7,12 @@ if __name__ == "__main__":
     from sys import argv
 
     monitor = Monitor.client(argv[1])
-    monitor.start_worker()
+    monitor.start_worker(update=False)
 
     client = Worker.client(argv[1])
     print(client.hello())
     sleep(4)
-    monitor.update_worker()
+    monitor.start_worker(update=True)
     client = Worker.client(argv[1])
     print(client.hello())
-    # print(client.send_file(data=Data(int(argv[3])), target_addr=argv[2]))
+    print(client.send_file(data=Data(int(argv[3])), target_addr=argv[2]))
